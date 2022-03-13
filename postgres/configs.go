424f896eda691bb519469b497b0b9f6ece7c0360
ljg-cqu <ljg_cqu@126.com>
2022-03-13 10:59:02 +0800

    add TestDBAliConnStr_ for ABFPaas Postgres testing db.

--- postgres/configs.go
+++ postgres/configs.go
@@ -1,7 +1,8 @@
 package postgres
 
 const (
-	TestDBAliConnStr = "user=zealy_test password=QrfV2_Pg host=pgm-bp1zf3qu5t76482qzo.pg.rds.aliyuncs.com port=5432 dbname=testdb sslmode=disable TimeZone=Asia/Shanghai"
+	TestDBAliConnStr  = "user=zealy_test password=QrfV2_Pg host=pgm-bp1zf3qu5t76482qzo.pg.rds.aliyuncs.com port=5432 dbname=testdb sslmode=disable TimeZone=Asia/Shanghai"
+	TestDBAliConnStr_ = "user=postgres password=123456 host=47.98.216.15 port=5432 dbname=test sslmode=disable TimeZone=Asia/Shanghai"
 )
 
 var (
